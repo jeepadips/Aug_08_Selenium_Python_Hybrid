3f18bf8876277cc94d05e869912c4e46c16d295e
Jeeva <jeepadips15@gmail.com>
2024-08-27 08:52:52 -0300

    Updated URL to OrangeCRM

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -6,16 +6,16 @@ from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
 
 class Test_001_Login:
-    baseURL = "https://admin-demo.nopcommerce.com/"
-    username = "admin@yourstore.com"
-    password = "admin"
+    baseURL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username = "Admin"
+    password = "admin123"
 
     def test_homePageTitle(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.baseURL)
         time.sleep(25)
         act_title = self.driver.title
-        if act_title == "Your store. Login":
+        if act_title == "OrangeHRM":
             assert True
             self.driver.close()
         else:
@@ -26,13 +26,13 @@ class Test_001_Login:
     def test_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.baseURL)
-        time.sleep(9)
+        time.sleep(25)
         self.lp=LoginPage(self.driver)
         self.lp.setUserName(self.username)
         self.lp.setPassword(self.password)
         self.lp.clickLogin()
         act_title=self.driver.title
-        if act_title == "Dashboard / nopCommerce administration":
+        if act_title == "OrangeHRM":
             assert True
             self.driver.close()
         else:
